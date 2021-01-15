52b45a6add1f8b53db69a9bed0235e93c54c3c48
Lorddistrict <etienne.crespi@laposte.net>
2021-01-15 17:08:55 +0100

    [Add] UPDATE related methods

--- src/main/java/com/example/testSpringBoot/student/StudentController.java
+++ src/main/java/com/example/testSpringBoot/student/StudentController.java
@@ -1,8 +1,10 @@
 package com.example.testSpringBoot.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -24,6 +26,17 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthdate,
+            @RequestParam(required = false) String email
+    ) {
+        studentService.updateStudent(studentId, firstName, lastName, birthdate, email);
     }
 
     @DeleteMapping(path = "{studentId}")
--- src/main/java/com/example/testSpringBoot/student/StudentService.java
+++ src/main/java/com/example/testSpringBoot/student/StudentService.java
@@ -3,7 +3,10 @@ package com.example.testSpringBoot.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -30,11 +33,41 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    @Transactional
+    public void updateStudent(Long studentId, String firstName, String lastName, LocalDate birthdate, String email) {
+
+        Student student = studentRepository.findById(studentId).orElseThrow(() ->
+            new IllegalStateException("Student ID doesn't exists")
+        );
+
+        if (firstName != null && firstName.length() > 0 && !Objects.equals(student.getFirstName(), firstName)) {
+            student.setFirstName(firstName);
+        }
+
+        if (lastName != null && lastName.length() > 0 && !Objects.equals(student.getLastName(), lastName)) {
+            student.setLastName(lastName);
+        }
+
+        if (birthdate != null && !Objects.equals(student.getBirthdate(), birthdate)) {
+            student.setBirthdate(birthdate);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email is already taken !");
+            }
+
+            student.setEmail(email);
+        }
+    }
+
     public void deleteStudent(Long studentId) {
         boolean  exists = studentRepository.existsById(studentId);
 
         if (!exists) {
-            throw new IllegalStateException("Student doesn't exists");
+            throw new IllegalStateException("Student ID doesn't exists");
         }
 
         studentRepository.deleteById(studentId);
