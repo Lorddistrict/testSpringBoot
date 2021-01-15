7e663a014de3bd3f15b38f6e1fb21a456f29eaec
Lorddistrict <etienne.crespi@laposte.net>
2021-01-15 11:33:38 +0100

    [Add] POST related methods & Enable messages from server

--- src/main/java/com/example/testSpringBoot/student/StudentService.java
+++ src/main/java/com/example/testSpringBoot/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -11,9 +12,21 @@ public class StudentService {
     private final StudentRepository studentRepository;
 
     @Autowired
-    public StudentService(StudentRepository studentRepository) { this.studentRepository = studentRepository; }
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void add(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email is already taken !");
+        }
+
+        studentRepository.save(student);
     }
 }
