b8aceb8a8006a586b0055c1d7e75d19eeac3ffb8
Lorddistrict <etienne.crespi@laposte.net>
2021-01-15 17:19:31 +0100

    [Edit] naming route

--- src/main/java/com/example/testSpringBoot/student/StudentController.java
+++ src/main/java/com/example/testSpringBoot/student/StudentController.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/student")
+@RequestMapping(path = "/api/v1/students")
 public class StudentController {
 
     private final StudentService studentService;
